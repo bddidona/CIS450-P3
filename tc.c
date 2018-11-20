fc8cc05ebb0181046d2abe8f26ac6e9ed3d4a2d1
Ben <bddidona@umich.edu>
2018-11-20 10:03:15 -0500

    Update tc.c

--- tc.c
+++ tc.c
@@ -1,1 +1,23 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+
+
+//Global stuff here
+
+
+void *tc_thread(void *arg) {
+
+
+}
+
+int main(int argh, char *argv[]) {
+
+	pthread_t thread;
+	
+	tc = pthread_create(&thread, NULL, tc_thread);
+	pthread_join(&thread);
+	
+	return 0;
+}
